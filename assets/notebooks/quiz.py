5da1efb015e55abdf6c37e8d72d075c9e7c3cfc5
AD <82819498+donze-informatikunterricht@users.noreply.github.com>
2021-05-18 16:14:07 +0200

    vorletzte änderungen

--- assets/notebooks/quiz.py
+++ assets/notebooks/quiz.py
@@ -100,7 +100,7 @@ Q5_arrays = create_multipleChoice_widget('Wie geben Sie eine Liste liste unter d
                                           'print(liste)'],
                                          'print(liste)')
 
-Q6_arrays = create_multipleChoice_widget('Was ist das Ergebnis folgender Rechnung: a+c[len(c)-3]*len(b)?',
+Q6_arrays = create_multipleChoice_widget('Was ist das Ergebnis der folgenden Rechnung: a+c[len(c)-3]*len(b)?',
                                          ['Keines, es gibt eine Fehlermeldung',
                                           '0',
                                           '5',
@@ -113,12 +113,12 @@ Q6_arrays = create_multipleChoice_widget('Was ist das Ergebnis folgender Rechnun
 
 ### Algorithmen
 
-Q1_algorithmen = create_multipleChoice_widget('Welche Alltagsaktivität',
+Q1_algorithmen = create_multipleChoice_widget('Welche dieser Aktivitäten ist eher kein Algorithmus?',
                                               ['Zähneputzen.',
                                                'Schuhe binden.',
                                                'Im Fussballspiel ein Goal schiessen.', 
-                                               '.'],
-                                              'Weil jederzeit klar sein muss, was zu tun ist.')
+                                               'Schwimmen.'],
+                                              'Im Fussballspiel ein Goal schiessen.')
 
 Q2_algorithmen = create_multipleChoice_widget('Warum ist es wichtig, einen Algorithmus genau zu beschreiben?',
                                               ['Weil er sonst macht, was er will.',
